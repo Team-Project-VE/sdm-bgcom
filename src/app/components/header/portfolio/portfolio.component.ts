e20248c48f3b63a09031c847f0558750b75c2f3d
EkMeh <barbaroca370@gmail.com>
2023-05-11 19:05:35 +0300

    update ++

--- src/app/components/header/portfolio/portfolio.component.ts
+++ src/app/components/header/portfolio/portfolio.component.ts
@@ -1,24 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from 'src/assets/i18n/products';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent implements  OnInit{
-  product: any;
+export class PortfolioComponent {
 
-  constructor(private route: ActivatedRoute) { }
 
   
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      const productId = Number(params.get('id'));
-      if (productId) {
-        this.product = products.find(p => p.id === productId);
-      }
-    });
-  }
+
 }
--- src/app/components/header/services/web-design/web-design.component.ts
+++ src/app/components/header/services/web-design/web-design.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from 'src/assets/i18n/products';
 
 @Component({
   selector: 'app-web-design',
@@ -9,16 +8,4 @@ import { products } from 'src/assets/i18n/products';
 })
 export class WebDesignComponent {
 
-  product: any;
-
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      const productId = Number(params.get('id'));
-      if (productId) {
-        this.product = products.find(p => p.id === productId);
-      }
-    });
-  }
 }
