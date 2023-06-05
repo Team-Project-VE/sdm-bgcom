5d5b1e9bcc811cd3be452bc28184f0ff8fefef0f
EkMeh <barbaroca370@gmail.com>
2023-06-05 19:36:07 +0300

    update all project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CoursesComponent } from './components/header/courses/courses.component';
 import { PortfolioComponent } from './components/header/portfolio/portfolio.component';
 import { ServicesComponent } from './components/header/services/services.component';
 import { OffersComponent } from './components/header/offers/offers.component';
 import { AboutUsComponent } from './components/header/about-us/about-us.component';
 import { ContactsComponent } from './components/header/contacts/contacts.component';
-import { PythonComponent } from './components/header/courses/python/python.component';
-import { CPlusPlusComponent } from './components/header/courses/c-plus-plus/c-plus-plus.component';
-import { FrontEndComponent } from './components/header/courses/front-end/front-end.component';
-import { BackEndComponent } from './components/header/courses/back-end/back-end.component';
-import { FullStackComponent } from './components/header/courses/full-stack/full-stack.component';
-import { InteriorDesignComponent } from './components/header/courses/interior-design/interior-design.component';
 import { WebDesignComponent } from './components/header/services/web-design/web-design.component';
 import { WebDevComponent } from './components/header/services/web-dev/web-dev.component';
 import { WebSupportComponent } from './components/header/services/web-support/web-support.component';
@@ -45,18 +38,11 @@ import { PoliciesComponent } from './components/policies/policies.component';
     HeaderComponent, // remove the conflict markers and keep this component
     MainComponent,
     FooterComponent,
-    CoursesComponent,
     PortfolioComponent,
     ServicesComponent,
     OffersComponent,
     AboutUsComponent,
     ContactsComponent,
-    PythonComponent,
-    CPlusPlusComponent,
-    FrontEndComponent,
-    BackEndComponent,
-    FullStackComponent,
-    InteriorDesignComponent,
     WebDesignComponent,
     WebDevComponent,
     WebSupportComponent,
